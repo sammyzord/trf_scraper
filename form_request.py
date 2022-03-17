d3f4c8bb431cff21e03033dfa60f3fb68643a458
Samuel Borges <borges.sb@protonmail.com>
2022-03-17 16:01:08 -0300

    :recycle: refactors example form request

--- form_request.py
+++ form_request.py
@@ -1,6 +1,34 @@
 import requests
 
-headers = {"Cache-Control": "no-cache"}
+cookies = {
+    "TRF1style": "default",
+    "PHPSESSID": "ioq92aq55enu0mai3ofcicr3t6",
+    "Qr1B4j3mrGeNWFYCqhmqt3LQGa1No9UGQ-77i7OniCmqfjjjIN6GvGu4swo_": "v1vtE+JQSDwNZ",
+}
+
+headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0"
+    ),
+    "Accept": (
+        "text/html,application/xhtml+xml,application/xml;"
+        "q=0.9,image/avif,image/webp,*/*;q=0.8"
+    ),
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": (
+        "https://processual.trf1.jus.br/consultaProcessual/nomeParte.php?secao=TRF1"
+    ),
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Origin": "https://processual.trf1.jus.br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-User": "?1",
+    "Cache-Control": "max-age=0",
+}
 
 params = (("secao", "TRF1"),)
 
@@ -9,18 +37,20 @@ data = {
     "secao": "TRF1",
     "pg": "2",
     "enviar": "Pesquisar",
+    "g-recaptcha-response": "",
+    "nmToken": "nomeParte",
 }
 
 response = requests.post(
     "https://processual.trf1.jus.br/consultaProcessual/parte/listar.php",
     headers=headers,
     params=params,
+    cookies=cookies,
     data=data,
 )
 
 filename = "test.txt"
 chunk_size = 100
-
 with open(filename, "wb") as fd:
     for chunk in response.iter_content(chunk_size):
         fd.write(chunk)
